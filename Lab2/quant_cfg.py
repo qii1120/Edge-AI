cf405a8f44f50ec71311afab1414d1f1cd5ee665
qii1120 <pingu901120@gmail.com>
2025-04-16 14:22:02 +0800

    finish section 2.2

--- Lab2/quant_cfg.py
+++ Lab2/quant_cfg.py
@@ -57,56 +57,37 @@ def get_quant_config_deit(model):
 
     return quant_config
 
-# def get_quant_config_deit(model):
-#     quant_config = {}
-
-#     q5 = BaseQuantizeConfig(nbits=5, group_size=32)
-#     q4 = BaseQuantizeConfig(nbits=4, group_size=16)
-#     q3 = BaseQuantizeConfig(nbits=3, group_size=16)
-#     q2 = BaseQuantizeConfig(nbits=2, group_size=16)
-#     q4_patch = BaseQuantizeConfig(nbits=4, group_size=32)
-
-#     n_blocks = len(model.blocks)
-
-#     for i in range(n_blocks):
-#         if i < 4:  # 前段：高精度
-#             quant_config[f'blocks.{i}.attn.qkv'] = q5
-#             quant_config[f'blocks.{i}.attn.proj'] = q5
-#             quant_config[f'blocks.{i}.mlp.fc1'] = q5
-#             quant_config[f'blocks.{i}.mlp.fc2'] = q5
-#         elif i < 8:  # 中段：中精度
-#             quant_config[f'blocks.{i}.attn.qkv'] = q4
-#             quant_config[f'blocks.{i}.attn.proj'] = q4
-#             quant_config[f'blocks.{i}.mlp.fc1'] = q4
-#             quant_config[f'blocks.{i}.mlp.fc2'] = q4
-#         else:  # 後段：壓縮精度
-#             quant_config[f'blocks.{i}.attn.qkv'] = q3
-#             quant_config[f'blocks.{i}.attn.proj'] = q3
-#             quant_config[f'blocks.{i}.mlp.fc1'] = q3
-#             quant_config[f'blocks.{i}.mlp.fc2'] = q2  # 精簡 fc2 精度空間
-
-#     # 加上 patch embedding 量化（若之前沒使用）
-#     quant_config['patch_embed.proj'] = q4_patch
-
-#     return quant_config
-
 
 
 # TODO: Make your own quant config for Language Model
 def get_quant_config_slm(model):
     quant_config = {}
     
-    # n_layers = model.config.num_hidden_layers
-    # q2_config = BaseQuantizeConfig(nbits=2, group_size=64) 
+    n_layers = model.config.num_hidden_layers
+    q4_32 = BaseQuantizeConfig(nbits=4, group_size=32) 
+    q4_64 = BaseQuantizeConfig(nbits=4, group_size=64) 
+    q8_64 = BaseQuantizeConfig(nbits=8, group_size=64)
+    q8_32 = BaseQuantizeConfig(nbits=8, group_size=32)
+    # print(f"Number of layers: {n_layers}")
     
-    # for i in range(n_layers):
-    #     quant_config[f'model.layers.{i}.self_attn.q_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.self_attn.k_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.self_attn.v_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.self_attn.o_proj'] = q2_config
-        
-    #     quant_config[f'model.layers.{i}.mlp.gate_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.mlp.up_proj'] = q2_config
-    #     quant_config[f'model.layers.{i}.mlp.down_proj'] = q2_config
+    for i in range(n_layers):
+        if i < (n_layers//4-1) or i > (n_layers*3//4+1):
+            quant_config[f'model.layers.{i}.self_attn.q_proj'] = q8_64
+            quant_config[f'model.layers.{i}.self_attn.k_proj'] = q8_64
+            quant_config[f'model.layers.{i}.self_attn.v_proj'] = q8_64
+            quant_config[f'model.layers.{i}.self_attn.o_proj'] = q8_64
+            
+            quant_config[f'model.layers.{i}.mlp.gate_proj'] = q8_64
+            quant_config[f'model.layers.{i}.mlp.up_proj'] = q8_64
+            quant_config[f'model.layers.{i}.mlp.down_proj'] = q8_64
+        else:
+            quant_config[f'model.layers.{i}.self_attn.q_proj'] = q4_32
+            quant_config[f'model.layers.{i}.self_attn.k_proj'] = q4_32
+            quant_config[f'model.layers.{i}.self_attn.v_proj'] = q4_64
+            quant_config[f'model.layers.{i}.self_attn.o_proj'] = q4_64
+            
+            quant_config[f'model.layers.{i}.mlp.gate_proj'] = q4_32
+            quant_config[f'model.layers.{i}.mlp.up_proj'] = q4_64
+            quant_config[f'model.layers.{i}.mlp.down_proj'] = q4_64
         
     return quant_config
